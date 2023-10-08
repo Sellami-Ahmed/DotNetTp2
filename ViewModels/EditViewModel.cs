df17e172d84dc3ff237274d35919eb05392082e2
Sellami-Ahmed <ahmed.sellami@enis.tn>
2023-10-08 21:20:03 +0100

    Add project files.

---
+++ ViewModels/EditViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TP2.ViewModels
+{
+    public class EditViewModel : CreateViewModel
+    {
+
+        public new int Id { get; set; }
+        public required string ExistingImagePath { get; set; }
+    }
+}
